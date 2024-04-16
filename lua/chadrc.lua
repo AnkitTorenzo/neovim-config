6c6992beea6521bdeb30d75c52553424560026d3
AnkitTorenzo <31123414+AnkitTorenzo@users.noreply.github.com>
2024-04-16 17:35:38 +0530

    email update

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -4,13 +4,23 @@
 ---@type ChadrcConfig
 local M = {}
 
+M.general = {
+  n = {
+    ["<C-h>"] = {"<cmd> TmuxNavigatorLeft<CR>", "window left" },
+    ["<C-l>"] = {"<cmd> TmuxNavigatorRight<CR>", "window right" },
+    ["<C-j>"] = {"<cmd> TmuxNavigatorDown<CR>", "window down" },
+    ["<C-k>"] = {"<cmd> TmuxNavigatorUp<CR>", "window up" },
+  }
+}
+
 M.ui = {
-	theme = "onedark",
+	theme = "catppuccin",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
 	-- },
 }
+
 
 return M
